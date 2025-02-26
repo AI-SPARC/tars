8c439a1b5ea9236add79a6e9e364809180108330
Geovane Filho <geovanelcfilho@gmail.com>
2025-02-26 14:11:15 -0300

    feat: finishing AGV CRUD

--- backend/api/v1/__init__.py
+++ backend/api/v1/__init__.py
@@ -1,1 +1,6 @@
-fastapi==0.115.8
+from fastapi import APIRouter
+from api.v1.agv.agv_routes import router as agv_router
+
+api_router = APIRouter()
+
+api_router.include_router(agv_router)
--- backend/db/schemas/agv_schema.py
+++
@@ -1,6 +0,0 @@
-from schemas.base import BaseSchema
-
-class AGVSchema(BaseSchema):
-    agv_id: int
-    manufacturer: str
-    serial_number: str
